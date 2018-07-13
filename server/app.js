c90df7f6abe7d015b94e6a8dfd8e7cab3f234ff8
Pragya <pragya@Pragyas-MacBook-Pro.local>
2018-07-14 01:58:14 +0530

    Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://pragya:test123@ds137661.mlab.com:37661/girls-who-javascript');
+mongoose.connection.once('open', () => {
+    console.log('Voila! Connected to database!');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -10,5 +16,5 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000, ()=> {
-    console.log('now listening to port 4000');
+    console.log('Hurray! Now listening to port 4000');
 });
