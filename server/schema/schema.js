61140732182c28159e26bc985be44b6d1778d366
Pragya <pragya@Pragyas-MacBook-Pro.local>
2018-07-14 01:33:15 +0530

    Added multiple speakers to a meetup in resolver

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,20 +10,31 @@ const {
 
 //dummy data
 var meetups = [
-    {name: 'GirlsWhoJS Meetup#1',date: 'July 1,2017', venue:'HasGeek', id: '1', speakerId :'1'},
-    {name: 'GirlsWhoJS Meetup#2',date: 'July 27,2017', venue:'Flipkart', id: '2', speakerId :'2'},
-    {name: 'GirlsWhoJS Meetup#3',date: 'Oct 7,2017', venue:'HasGeek', id: '3', speakerId :'3'},
-    {name: 'GirlsWhoJS Meetup#4',date: 'Dec 17,2017', venue:'HasGeek', id: '4', speakerId :'1'},
-    {name: 'GirlsWhoJS Meetup#5',date: 'March 11,2018', venue:'Treebo', id: '5', speakerId :'2'},
-    {name: 'GirlsWhoJS Meetup#6',date: 'May 6,2018', venue:'Treebo', id: '6', speakerId :'3'},
-    {name: 'GirlsWhoJS Meetup#7',date: 'June 10,2018', venue:'HasGeek', id: '7', speakerId :'1'},
-    {name: 'GirlsWhoJS Meetup#8',date: 'July 15,2018', venue:'Treebo', id: '8', speakerId :'2'}
+    {name: 'GirlsWhoJS Meetup#1',date: 'July 1,2017', venue:'HasGeek', id: '1', speakerIds :['1','7']},
+    {name: 'GirlsWhoJS Meetup#2',date: 'July 27,2017', venue:'Flipkart', id: '2', speakerIds :['4','11']},
+    {name: 'GirlsWhoJS Meetup#3',date: 'Oct 7,2017', venue:'HasGeek', id: '3', speakerIds :['2','5']},
+    {name: 'GirlsWhoJS Meetup#4',date: 'Dec 17,2017', venue:'HasGeek', id: '4', speakerIds :['1','3','12']},
+    {name: 'GirlsWhoJS Meetup#5',date: 'March 11,2018', venue:'Treebo Hotels', id: '5', speakerIds :['6','8']},
+    {name: 'GirlsWhoJS Meetup#6',date: 'May 6,2018', venue:'Treebo Hotels', id: '6', speakerIds :['10','14']},
+    {name: 'GirlsWhoJS Meetup#7',date: 'June 10,2018', venue:'HasGeek', id: '7', speakerIds :['6','9','13']},
+    {name: 'GirlsWhoJS Meetup#8',date: 'July 15,2018', venue:'Treebo Hotels', id: '8', speakerIds :['1','8','12']}
 ];
 
 var speakers = [
     {name: 'Adisha Porwal', company:'TypeSet.io', id: '1'},
-    {name: 'Pragya Jha', company:'Treebo Hotels', id: '2'},
-    {name: 'Toshi Gupta', company:'Flipkart', id: '3'},
+    {name: 'Anbarasi U', company:'Tesco', id: '2'},
+    {name: 'Aparna', company:'Artoo', id: '3'},
+    {name: 'Franziska Hinkelmann', company:'Google', id: '4'},
+    {name: 'Manjula Dube', company:'BookMyShow', id: '5'},
+    {name: 'Nishtha Gupta', company:'Flipkart', id: '6'},
+    {name: 'Pavithra', company:'Atlassian', id: '7'},
+    {name: 'Pragya Jha', company:'Treebo Hotels', id: '8'},
+    {name: 'Rakhi Sharma', company:'The Data Team', id: '9'},
+    {name: 'Ruchi Mutneja', company:'Quintype', id: '10'},
+    {name: 'Sarah Drasner', company:'Microsoft', id: '11'},
+    {name: 'Toshi Gupta', company:'Flipkart', id: '12'},
+    {name: 'Varsha Bhagat', company:'Treebo Hotels', id: '13'},
+    {name: 'Vidya Ramakrishnan', company:'HasGeek', id: '14'},
 ];
 
 const MeetupType = new GraphQLObjectType({
@@ -33,11 +44,13 @@ const MeetupType = new GraphQLObjectType({
         name: {type : GraphQLString},
         date: {type : GraphQLString},
         venue: {type : GraphQLString},
-        speaker: {
-            type: SpeakerType,
+        speakers: {
+            type: new GraphQLList(SpeakerType),
             resolve(parent,args){
                 console.log(parent);
-                return _.find(speakers, {id: parent.speakerId})
+                return parent.speakerIds.map((speakerId) => 
+                    _.find(speakers, {id: speakerId})
+                )
             }
         }
     })
