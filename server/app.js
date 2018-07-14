fc81eab0530bf128c72f3b7e0d5d6c19d45593d0
Pragya <pragya@Pragyas-MacBook-Pro.local>
2018-07-14 16:37:33 +0530

    Allowing cross-origin requests

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());// allow cross-origin requests
 mongoose.connect('mongodb://pragya:test123@ds137661.mlab.com:37661/girls-who-javascript');
 mongoose.connection.once('open', () => {
     console.log('Voila! Connected to database!');
